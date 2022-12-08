6b4e13b451cdcf8d8cd60f1271a6c798c920f98b
nicapos <annikaposadas22@gmail.com>
2022-12-08 15:22:15 +0800

    Add Client, Server class
    add Client and Server class
    
    bug: no error message when server is down

--- client.py
+++ client.py
@@ -1,59 +1,113 @@
-import socket
-def parse_input(input_string):
-    command, *params = input_string.split()
+import socket, json
 
-    if command == '/join':
-        # Connect to the server application
-        # Syntax: /join <server_ip_add> <port>
-        try:
-            server_ip_add, port = params
-            
-            # socket object
-            print ("Connecting to Host: "+server_ip_add+" in Port: "+ port+"...")
-            try:
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((server_ip_add, int(port)))    
-            except:
-                print("Can't connect to server...")
-                
-        except:
-            print("Error: Command parameters do not match or is not allowed.")
+class Client:
+    def __init__(self):
+        self.handle = None
+        self.socket = None
+        self.server_address_port = None
 
-    elif command == 'leave':
-        # Disconnect to the server application
-        pass
+    def send(self, message):
+        # Send message to server
+        client_message = json.dumps(message)   # convert message to JSON
+        self.socket.sendto(client_message.encode(), self.server_address_port)
 
-    elif command == '/register':
-        # Register a unique handle or alias
-        # Syntax: /register <handle>
-        try:
-            [handle] = params
-        except:
-            print("Error: Command parameters do not match or is not allowed.")
+        #server_message, *_ = self.socket.recvfrom(1024)
+        #return server_message.decode()
 
-    elif command == '/all':
-        # Send message to all
-        # Syntax: /all <message>
-        message = ' '.join(params)
+    def connect(self, socket_address):
+        # Connect to server
+        self.server_address_port = socket_address
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-    elif command == '/msg':
-        # Send direct message to a single handle
-        # Syntax: /msg <handle> <message>
-        try:
-            handle = params[0]
-            message = ' '.join(params[1:])
-        except:
-            print("Error: Command parameters do not match or is not allowed.")
+    def disconnect(self):
+        # Disconnect from server
+        self.socket.close()
 
-    elif command == '/?':
-        # Request command help to output all Input, Syntax commands for references
-        pass
+    def register(self, handle):
+        # Registers user's handle. Assumes the handle is unique. Check with server first before calling this function.
+        self.handle = handle
 
-    else:
-        print("Error: Command not found.")
 
 
 if __name__ == "__main__":
+    client = Client()
+
     while True:
-        user_input = input('>> ')
-        parse_input(user_input)
+        user_input = input('\n>> ')
+        command, *params = user_input.split()
+
+        if command == '/join':
+            # Connect to the server application
+            # Syntax: /join <server_ip_add> <port>
+            try:
+                server_ip_add, port = params
+                
+                try:
+                    client.connect( (server_ip_add, int(port)) )
+                    client.send({"command": "join"})
+                    print("Connection to the Message Board Server is successful!")
+                except Exception as e:
+                    print(e)
+                    print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+            except:
+                print("Error: Command parameters do not match or is not allowed.")
+
+
+        elif command == '/leave':
+            # Disconnect to the server application
+            try:
+                client.send({"command": "leave"})
+                client.disconnect()
+                print('Connection closed. Thank you!')
+            except:
+                print("Error: Disconnection failed. Please connect to the server first.")
+
+
+        elif command == '/register':
+            # Register a unique handle or alias
+            # Syntax: /register <handle>
+            try:
+                [handle] = params
+
+                try:
+                    client.send({"command": "register", "handle": handle})
+                except:
+                    print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+            except:
+                print("Error: Command parameters do not match or is not allowed.")
+
+
+        elif command == '/all':
+            # Send message to all
+            # Syntax: /all <message>
+            message = ' '.join(params)
+
+            try:
+                client.send({"command": "all", "message": message})
+            except:
+                print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+
+
+        elif command == '/msg':
+            # Send direct message to a single handle
+            # Syntax: /msg <handle> <message>
+            try:
+                handle = params[0]
+                message = ' '.join(params[1:])
+
+                try:
+                    client.send({"command": "msg", "handle": handle, "message": message})
+                except:
+                    print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+
+            except:
+                print("Error: Command parameters do not match or is not allowed.")
+
+
+        elif command == '/?':
+            # Request command help to output all Input, Syntax commands for references
+            pass    # TODO
+
+
+        else:
+            print("Error: Command not found.")
--- server.py
+++ server.py
@@ -1,25 +1,29 @@
 #server
 import socket
 
-def start_server():
-    HOST = socket.gethostbyname(socket.gethostname())
-    PORT = 8080
-    
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server_socket.bind((HOST,PORT))
-        print("Server listeng to "+HOST+" "+str(PORT)+"...")
-    except:
-        print("Error binding server...")
-    
-    server_socket.listen(1)
-    
-    while True:
-        # establish a connection
-        client_socket, addr = server_socket.accept()
+class Server:
+    def __init__(self, host:str, port:int):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind( (host, port) )
 
-        print("Got a connection from %s" % str(addr))
-        message = client_socket.recv(1024).decode('ascii')
+    def listen(self):
+        message, client_ip = self.socket.recvfrom(1024)
+
+        # DEBUG: to check if narreceive ba talaga
+        print(f'Client {client_ip} sent:')
+        print(message.decode())
+        print()
+
+    def start(self):
+        print(f"Server listening to {self.host} {self.port}...")
+        while True:
+            self.listen()
 
 if __name__ == "__main__":
-    start_server()
+    HOST    = socket.gethostname()
+    PORT    = 8080
+
+    server = Server(HOST, PORT)
+    server.start()
