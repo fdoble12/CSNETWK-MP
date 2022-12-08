6b5f06e251e2ff8fe1ba795271fe753813afeaf9
nicapos <annikaposadas22@gmail.com>
2022-12-08 21:00:22 +0800

    Add send to all
    not working yet for client huhu

--- server.py
+++ server.py
@@ -16,8 +16,19 @@ class Server:
         # Returns the list of handles of clients connected to the server
         return self.directory.keys()
 
+    def get_handle(self, client_address):
+        # Returns the client's handle given their address
+        for handle, address in self.directory.items():
+            if address == client_address:
+                return handle
+
+    def get_address(self, handle):
+        # Returns the client's address given their handle
+        return self.directory[handle]
+
 
     def send_response(self, response, client_address):
+        # Send message to client (JSON)
         server_message = json.dumps( {"message": response} ).encode('ascii')
         self.socket.sendto(server_message, client_address)
 
@@ -27,8 +38,8 @@ class Server:
        
         while True:
             # Listen for client requests
-            message, client_address = self.socket.recvfrom(1024)
-            payload = json.loads(message.decode())
+            client_message, client_address = self.socket.recvfrom(1024)
+            payload = json.loads(client_message.decode())
             
             print(f"client #{client_address}: \t{payload}") # DEBUG: to check if narreceive ba talaga
 
@@ -36,8 +47,11 @@ class Server:
             if command == 'join':
                 self.send_response("Connection to Message Board Server is successful!", client_address)
 
+
             elif command == 'leave':
+                self.directory.pop(client_address)  # Remove client from active users
                 self.send_response("Connection closed. Thank you!", client_address)
+
 
             elif command == '/register':
                 handle = payload['handle']
@@ -48,11 +62,21 @@ class Server:
                     self.directory[handle] = client_address
                     self.send_response(f"Welcome {handle}!", client_address)
 
+
             elif command == '/all':
-                pass    # TODO
+                client_handle = self.get_handle(client_address)
+
+                if client_handle in self.active_users:
+                    message = payload['message']
+
+                    for address in self.active_users:
+                        self.send_response(message, address)
+                else:
+                    self.send_response("Error: ", address)  # TODO: Add error message for unregistered users. Wala sa specs?
+
 
             elif command == '/msg':
-                pass    # TODO
+                pass # TODO
 
 
 if __name__ == "__main__":
