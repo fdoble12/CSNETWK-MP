387731561c3af2eb83f0e0cf99f564641588fcf3
nicapos <annikaposadas22@gmail.com>
2022-12-08 20:34:38 +0800

    Add register

--- server.py
+++ server.py
@@ -3,38 +3,58 @@ import socket, json
 
 class Server:
     def __init__(self, host:str, port:int):
+        self.directory = {}     # directory of clients. key = handle, value = client address
+
         self.host = host
         self.port = port
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind( (host, port) )
 
-    def listen(self):
-        message, client_ip = self.socket.recvfrom(1024)
-        payload = json.loads(message.decode())
-        self.respond(payload, client_ip)
-       
+    @property
+    def active_users(self):
+        # Returns the list of handles of clients connected to the server
+        return self.directory.keys()
 
-        # DEBUG: to check if narreceive ba talaga
-        print(f'Got a connection from {client_ip}:')
-        print("Client Command: "+payload['command'])
-        print(payload)
-        print()        
+
+    def send_response(self, response, client_address):
+        server_message = json.dumps( {"message": response} ).encode('ascii')
+        self.socket.sendto(server_message, client_address)
+
 
     def start(self):
         print(f"Server listening to {self.host} {self.port}...")
+       
         while True:
-            self.listen()
-
-    def respond(self, payload, client_address):
-        response = {"message":""}
-        command = payload['command']
-        if command == "join":
-            response['message'] = "Connection to Mssage Board Server is successful!"
-            self.socket.sendto(json.dumps(response).encode('ascii'), client_address)
-        elif command == "leave":
-            response['message'] = "Connection closed. Thank you!"
-            self.socket.sendto(json.dumps(response).encode('ascii'), client_address)
+            # Listen for client requests
+            message, client_address = self.socket.recvfrom(1024)
+            payload = json.loads(message.decode())
             
+            print(f"client #{client_address}: \t{payload}") # DEBUG: to check if narreceive ba talaga
+
+            command = payload['command']
+            if command == 'join':
+                self.send_response("Connection to Message Board Server is successful!", client_address)
+
+            elif command == 'leave':
+                self.send_response("Connection closed. Thank you!", client_address)
+
+            elif command == '/register':
+                handle = payload['handle']
+
+                if handle in self.active_users:
+                    self.send_response("Error: Registration failed. Handle or alias already exists.", client_address)
+                else:
+                    self.directory[handle] = client_address
+                    self.send_response(f"Welcome {handle}!", client_address)
+
+            elif command == '/all':
+                pass    # TODO
+
+            elif command == '/msg':
+                pass    # TODO
+
+
 if __name__ == "__main__":
     HOST    = socket.gethostname()
     PORT    = 8080
