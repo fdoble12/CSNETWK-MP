d8bb785664575baf6aa1e42db4abd811f42a5479
nicapos <annikaposadas22@gmail.com>
2022-12-08 16:11:39 +0800

    Resolve merge again

--- server.py
+++ server.py
@@ -10,18 +10,25 @@ class Server:
 
     def listen(self):
         message, client_ip = self.socket.recvfrom(1024)
+        payload = json.loads(message.decode())
+        self.respond(payload, client_ip)
+       
 
         # DEBUG: to check if narreceive ba talaga
         print(f'Got a connection from {client_ip}:')
-        payload = json.loads(message.decode())
         print("Client Command: "+payload['command'])
         print(payload)
-        print()
+        print()        
 
     def start(self):
         print(f"Server listening to {self.host} {self.port}...")
         while True:
             self.listen()
+
+    def respond(self, payload, client_address):
+        if payload['command'] == "join":
+            message = "Connection to Message Board Server is successful!"
+            self.socket.sendto(message.encode(), client_address)    # TODO: Send response to client as JSON
 
 if __name__ == "__main__":
     HOST    = socket.gethostname()
