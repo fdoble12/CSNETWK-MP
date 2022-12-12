05595d7281e34ab4df51841182c38c8b74e5dcd2
nicapos <annikaposadas22@gmail.com>
2022-12-12 18:49:03 +0800

    Move chat to GUI

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket, json
 from multiprocessing import Process
 from tkinter import *
+from threading import *
 
 
 class Client:
@@ -8,26 +9,31 @@ class Client:
         self.handle = None
         self.socket = None
         self.server_address_port = None
+
+        # thread for server responses
+        self.t = Thread(target=self.listen)
+        self.activeThread = False
         
+        # GUI
         self.root = master
         self.create_gui()
 
+        self.gui_print("***** Welcome to  Message Board System *****")
+        self.gui_print("type \"/?\" for the commands")
     
     def send(self, message):
         # Send message to server
         client_message = json.dumps(message)   # convert message to JSON
         self.socket.sendto(client_message.encode(), self.server_address_port)
         
-
     def server_message(self):
         return self.socket.recv(1024).decode('ascii')
 
     def listen(self):
         while True:
-            if self.socket:
-                encoded_message, *_ = self.socket.recvfrom(1024)
-                response = json.loads(encoded_message.decode('ascii'))
-                print(response['message'] + '\n')
+            encoded_message, *_ = self.socket.recvfrom(1024)
+            response = json.loads(encoded_message.decode('ascii'))
+            self.gui_print(response['message'])
 
     def connect(self, socket_address):
         # Connect to server
@@ -48,44 +54,41 @@ class Client:
         self.root.geometry('400x500')
 
         #header
-        head_label = Label(root, bg="white", text=" Welcome to Message Board System ",  font=("Arial", 13), pady=10)
-        head_label.place(relwidth=1)
+        self.head_label = Label(root, bg="white", text=" Welcome to Message Board System ",  font=("Arial", 13), pady=10)
+        self.head_label.place(relwidth=1)
 
         #create chat window
-        chatwindow = Text(root, bg= "beige", width= 50, height= 8)
-        chatwindow.place(x= 15, y= 50, height= 330, width= 370)
+        self.chatwindow = Text(root, bg= "beige", width= 50, height= 8)
+        self.chatwindow.place(x= 15, y= 50, height= 330, width= 370)
 
         #create message area
-        messageWindow = Text(root, bd= 3, bg= "beige", width= 30, height= 4)
-        messageWindow.place (x= 15, y= 400, height= 88, width= 260)
+        self.messageWindow = Text(root, bd= 3, bg= "beige", width= 30, height= 4)
+        self.messageWindow.place (x= 15, y= 400, height= 88, width= 260)
 
         #button for sending
-        button = Button(root, text = "Enter", bg= "white", activebackground = "light blue", font=("Arial", 13), width = 5, height = 5)
-        button.place(x= 285, y= 400, height= 88, width= 100)
+        self.button = Button(root, text = "Enter", bg= "white", activebackground = "light blue", font=("Arial", 13), width = 5, height = 5, command=self.exec)
+        self.button.place(x= 285, y= 400, height= 88, width= 100)
 
         #scroll bar
-        scroll = Scrollbar(root, command=chatwindow.yview())
-        scroll.place(x= 375, y=50, height= 330)
+        self.scroll = Scrollbar(root, command=self.chatwindow.yview())
+        self.scroll.place(x= 375, y=50, height= 330)
+
+        #listen to enter key for commands
+        self.root.bind('<Return>', self.exec)
 
         # self.win.protocol("WM_DELETE_WINDOW", self.stop)
 
-    
+    def gui_print(self, text):
+        self.chatwindow.insert(END, text + '\n\n')
 
-if __name__ == "__main__":
-    root = Tk()
-    client = Client(root)
-    proc = Process(target=client.listen)
+    def exec(self, evt=None):
+        user_input = self.messageWindow.get("1.0",'end-1c')
 
-    print("***** Welcome to  Message Board System *****")
-    print("\ntype \"/?\" for the commands")
-
-    root.send
-
-    try:
-        while True:
-            user_input = input()
+        if user_input.strip():  # check if input box is blank
             command, *params = user_input.split()
-            
+
+            # reset input field
+            self.messageWindow.delete(1.0, END)
 
             if command == '/join':
                 # Connect to the server application
@@ -94,22 +97,25 @@ if __name__ == "__main__":
                     server_ip_add, port = params
                     
                     try:
-                        client.connect( (server_ip_add, int(port)) )
-                        client.send({"command": "join"})
-                        proc.start()
+                        self.connect( (server_ip_add, int(port)) )
+                        self.send({"command": "join"})
+
+                        if not self.activeThread:
+                            self.t.start()
+                            self.activeThread = True
                     except:
-                        print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+                        self.gui_print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
                 except:
-                    print("Error: Command parameters do not match or is not allowed.")
+                    self.gui_print("Error: Command parameters do not match or is not allowed.")
 
 
             elif command == '/leave':
                 # Disconnect to the server application
                 try:
-                    client.send({"command": "leave"})
-                    client.disconnect()
+                    self.send({"command": "leave"})
+                    self.disconnect()
                 except:
-                    print("Error: Disconnection failed. Please connect to the server first.")
+                    self.gui_print("Error: Disconnection failed. Please connect to the server first.")
 
 
             elif command == '/register':
@@ -119,11 +125,11 @@ if __name__ == "__main__":
                     [handle] = params
 
                     try:
-                        client.send({"command": "register", "handle": handle})
+                        self.send({"command": "register", "handle": handle})
                     except:
-                        print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+                        self.gui_print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
                 except:
-                    print("Error: Command parameters do not match or is not allowed.")
+                    self.gui_print("Error: Command parameters do not match or is not allowed.")
 
 
             elif command == '/all':
@@ -132,9 +138,9 @@ if __name__ == "__main__":
                 message = ' '.join(params)
 
                 try:
-                    client.send({"command": "all", "message": message})
+                    self.send({"command": "all", "message": message})
                 except:
-                    print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+                    self.gui_print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
 
 
             elif command == '/msg':
@@ -145,29 +151,38 @@ if __name__ == "__main__":
                     message = ' '.join(params[1:])
 
                     try:
-                        client.send({"command": "msg", "handle": handle, "message": message})
+                        self.send({"command": "msg", "handle": handle, "message": message})
                     except:
-                        print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
+                        self.gui_print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
 
                 except:
-                    print("Error: Command parameters do not match or is not allowed.")
+                    self.gui_print("Error: Command parameters do not match or is not allowed.")
 
 
             elif command == '/?':
                 # Request command help to output all Input, Syntax commands for references
-                print("\n*************************** Commands for references ***************************")
-                print("\t/join <server_ip_add> <port>")
-                print("\t/leave")
-                print("\t/register <handle>")
-                print("\t/all <message>")
-                print("\t/msg <handle> <message>")
+                self.gui_print("************* Commands for references *************\n" + \
+                "/join <server_ip_add> <port>    Connect to the server application\n" + \
+                "/leave                          Disconnect to the server application\n" + \
+                "/register <handle>              Register a unique handle or alias\n" + \
+                "/all <message>                  Send message to all\n" + \
+                "/msg <handle> <message>         Send direct message to a single handle\n""")
 
             else:
-                print("Error: Command not found.")
+                self.gui_print("Error: Command not found.")
 
+
+
+if __name__ == "__main__":
+    root = Tk()
+    client = Client(root)
+
+    root.send
+
+    try:
+        root.mainloop()
     except KeyboardInterrupt:
         # Disconnect client before exiting program
         if client.socket != None:
             client.send({"command": "leave"})
-            client.disconnect()
-            proc.terminate()
+            client.t.disconnect()
